3d53eef645a95fe111dbb5d2e25e10d54ac0d017
Sairam <sairam839@gmail.com>
2025-09-25 16:18:25 +0530

    Name

--- 02-Conversation.sh
+++ 02-Conversation.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "Ramesh:: Hi Suresh, How are you?"
-echo "Suresh:: Hello Ramesh, I am fine, How are you"
+
+PERSON1=John
+PERSON2=Rahim
+
+echo "PERSON1:: Hi PERSON2, How are you?"
+echo "PERSON2:: Hello PERSON1, I am fine, How are you"
